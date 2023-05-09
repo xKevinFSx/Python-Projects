7df394d50398883a8ad3d2b2de8bc9f38c7be0ed
Kevin Farias <kevin.fs2013@gmail.com>
2023-05-09 20:16:28 -0300

    Primeiro commit

---
+++ PasswordGenerate.py
@@ -0,0 +1,28 @@
+import random
+import string
+
+print('Bem vindo ao gerador de senhas')
+
+#string com todas as letras
+letras = string.ascii_letters
+#string com todos os numeros
+digitos = string.digits
+#string com todos os caracteres especiais
+caractes_especiais = string.punctuation
+
+#concatenar string
+alfabeto = letras + digitos + caractes_especiais
+
+numero = input('Insita a quantidade de senhas a serem geradas: ')
+numero = int(numero)
+
+tamanho = input("Insira o tamanho que a(s) senha(s) devem ter: ")
+tamanho = int(tamanho)
+
+print('\nAqui está todas as senhas geradas:')
+
+for sna in range(numero):
+    senhas = ''
+    for c in range(tamanho):
+        senhas += random.choice(alfabeto)
+    print(senhas)
