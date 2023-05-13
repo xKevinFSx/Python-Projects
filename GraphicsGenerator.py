098617bc7822d9f03e1c161cc49466fc45bdffb2
Kevin Farias <kevin.fs2013@gmail.com>
2023-05-13 18:38:56 -0300

    Primeito commit/Conexão com banco de dados

---
+++ GraphicsGenerator.py
@@ -0,0 +1,27 @@
+import mysql.connector
+import config
+from mysql.connector import errorcode
+
+
+#Conexão com banco de dados
+try:
+    cnx = mysql.connector.connect(user=config.user, password=config.password, database=config.database)
+    if cnx.is_connected():
+        print('Conexão com banco de dados feita com sucesso')
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print('Nome de usuario ou senha estão incorretos')
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print('Banco de dados não existe')
+    else:
+        print(err)
+#else:
+    #cnx.close()
+
+#Fazer consulta no banco de dados através do cursor    
+cursor = cnx.cursor()
+
+query = ('SELECT name, district, population FROM city')
+         #'ORDER BY Population DESC LIMIT 10')
+
+cursor.execute(query)
