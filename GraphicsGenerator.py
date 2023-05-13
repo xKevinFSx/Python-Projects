cb265bb2e8a9aa03763b093df844a2c742662b72
Kevin Farias <kevin.fs2013@gmail.com>
2023-05-13 19:14:30 -0300

    Criação das querys

--- GraphicsGenerator.py
+++ GraphicsGenerator.py
@@ -8,6 +8,18 @@ try:
     cnx = mysql.connector.connect(user=config.user, password=config.password, database=config.database)
     if cnx.is_connected():
         print('Conexão com banco de dados feita com sucesso')
+        
+        #Fazer consulta no banco de dados através do cursor    
+        cursor = cnx.cursor()
+
+        query = ('SELECT name, population FROM city ORDER BY Population DESC LIMIT 10')
+
+        cursor.execute(query)
+        resultado = cursor.fetchall()
+        
+        for i in resultado:
+            print(i)
+        
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
         print('Nome de usuario ou senha estão incorretos')
@@ -15,13 +27,5 @@ except mysql.connector.Error as err:
         print('Banco de dados não existe')
     else:
         print(err)
-#else:
-    #cnx.close()
-
-#Fazer consulta no banco de dados através do cursor    
-cursor = cnx.cursor()
-
-query = ('SELECT name, district, population FROM city')
-         #'ORDER BY Population DESC LIMIT 10')
-
-cursor.execute(query)
+else:
+    cnx.close()
